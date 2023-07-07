1e4a092e8fbc670dcc0f0e6db7371bf045b46eba
Nam HyeonWoo <nhu0314@naver.com>
2023-07-07 12:18:02 +0900

    feat: 게스트 로그인, 자동 로그인 기능 추가

--- 01_Title/Temp_GuestLogin.cs
+++ 01_Title/Temp_GuestLogin.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Temp_GuestLogin : MonoBehaviour
 {
-    private void Start()
+    public void OnClickStartGuest()
     {
+        var bro = BackEnd.Backend.BMember.GuestLogin();
         
+        if(bro.IsSuccess())
+        {
+            Debug.Log("게스트 로그인 성공!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene JG");
+        }
+        else
+        {
+            Debug.LogError($"게스트 로그인 실패 : {bro}");
+        }
     }
 
-    private void Update()
+    public void OnClickTokenLogin()
     {
-        
+        var bro = BackEnd.Backend.BMember.LoginWithTheBackendToken();
+
+        if(bro.IsSuccess())
+        {
+            Debug.Log("자동 로그인에 성공했습니다");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene JG");
+        }
+        else
+        {
+            Debug.LogError($"자동 로그인 실패 : {bro}");
+        }
+    }
+
+    public void OnClickLogout()
+    {
+        BackEnd.Backend.BMember.Logout();
+        // 액세스 토큰 삭제, 즉 토큰 로그인 불가 (로그인이 먼저 되어야함)
+    }
+
+    public void OnClickDeleteData()
+    {
+        BackEnd.Backend.BMember.DeleteGuestInfo();
+        // 서버 측 데이터는 삭제 안됨
     }
 }
