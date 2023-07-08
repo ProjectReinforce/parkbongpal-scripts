8356d56113e12d6547d65ee1bab0c9468c8a74ce
IJH_97 <dryer97@naver.com>
2023-07-08 14:20:41 +0900

    feat : 풀 받음, 로딩씬 구현, 로딩 스크립트 구현

---
+++ SceneLoader.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private TextMeshProUGUI persentMessage;
+    private static string targetScene;
+    void Start()
+    {
+        StartCoroutine(SceneLoading());
+    }
+
+    public static void LoadScene(string _sceneName)
+    {
+        targetScene = _sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    private IEnumerator SceneLoading()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(targetScene);
+        operation.allowSceneActivation = false;
+
+        float timer = 0f;
+        while(!operation.isDone)
+        {
+            yield return null;
+            if( operation.progress < 0.9f )
+            {
+                progressBar.value = operation.progress;//Mathf.MoveTowards(progressBar.value,0.9f,Time.deltaTime);
+                persentMessage.text = (System.Math.Round(operation.progress, 4) * 100).ToString() + "%";
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                progressBar.value = Mathf.Lerp(0.9f, 1f, timer);
+                persentMessage.text = (System.Math.Round(Mathf.Lerp(0.9f, 1f, timer), 4) * 100).ToString() + "%";
+                if( progressBar.value >= 1f )
+                {
+                    operation.allowSceneActivation = true;
+                    break;
+                }
+            }
+            // else if ( operation.progress >= 0.9f )
+            // {
+            //     progressBar.value = Mathf.MoveTowards(progressBar.value,1f,Time.deltaTime);
+            // }
+            // else
+            // {
+            //     progressBar.value = Mathf.Lerp(0.9f, 1f, Time.deltaTime);
+            //     persentMessage.text = (System.Math.Round(Mathf.Lerp(0.9f, 1f, Time.deltaTime), 4) * 100).ToString() + "%";
+            //     if( progressBar.value >= 1f )
+            //     {
+            //         operation.allowSceneActivation = true;
+            //         break;
+            //     }
+            // }
+        }
+        
+    }
+}
