f08fdb99759a29d7320c96d871e38c919803ac14
KongJin <pibrod@gmail.com>
2023-07-09 12:13:37 +0900

    feat: 무기 데이터 컨테이닝

--- 00_Common/Structs.cs
+++ 00_Common/Structs.cs
@@ -1,8 +1,32 @@
 ﻿using System;
 
-public struct WeaponStat
+public struct WeaponStat// 유저마다 바뀔수 있는 데이터
 {
-    public float damage, speed, range, accuracy;
+    public int id;
+    public int damage, speed, range, accuracy, grade, inventoryIndex;
+
+    public int normalReinforceCount;
+
+   
+    public WeaponStat(int _id, int _damage,int _speed, int _range,int _accuracy,int _grade,int _inventoryIndex, int _normalReinforceCount=0)
+    {
+        id = _id;
+        damage = _damage;
+        speed = _speed;
+        range = _range;
+        accuracy = _accuracy;
+        
+        grade = _grade;
+        inventoryIndex = _inventoryIndex;
+        
+        normalReinforceCount = _normalReinforceCount;
+    }
+    public override string ToString()
+    {
+        
+        return string.Format("id: {0} damage: {1} speed: {2} range: {3} accuracy: {4} grade: {5}",
+            id, damage, speed, range, accuracy, grade);
+    }
 }
 
 public struct OreStat
---
+++ 03_Main/BackTest.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BackEnd;
+using LitJson;
+using UnityEngine;
+
+
+public class BackTest : MonoBehaviour
+{
+    private Action[] test =
+    {
+        () =>
+        {
+            var bro = BackEnd.Backend.BMember.GuestLogin();
+        
+            if(bro.IsSuccess())
+            {
+                Debug.Log("게스트 로그인 성공!");
+            }
+            else
+            {
+                Debug.LogError($"게스트 로그인 실패 : {bro}");
+            }
+        },
+        () =>
+        {
+            var bro = BackEnd.Backend.BMember.LoginWithTheBackendToken();
+
+            if(bro.IsSuccess())
+            {
+                Debug.Log("자동 로그인에 성공했습니다");
+            }
+            else
+            {
+                Debug.LogError($"자동 로그인 실패 : {bro}");
+            }
+        },
+        () =>
+        {
+            Backend.BMember.Logout();
+        },
+        () =>
+        {
+            Backend.BMember.DeleteGuestInfo();
+        },
+        () =>
+        {
+            Param param = new Param();
+
+            param.Add("id", 2);
+            param.Add("damage", 4);
+            param.Add("speed", 5);
+            param.Add("range", 1);
+            param.Add("accuracy", 2);
+            param.Add("grade", 2);
+            param.Add("inventoryIndex", 2);
+            //param.Add("normalReinforceCount", 1);
+            
+            SendQueue.Enqueue(Backend.GameData.Insert, "weaponTest_JG", param, (callback) => 
+            {
+                Debug.Log("gg");
+                Debug.Log(callback);
+                // 이후 처리
+            });
+            
+        },
+        () =>//5
+        {
+            SendQueue.Enqueue(Backend.GameData.Get, "weaponTest_JG", new Where(), 10, bro =>
+            {
+                if (bro.IsSuccess() == false)
+                {
+                    // 요청 실패 처리
+                    Debug.Log(bro);
+                    return;
+                }
+                JsonData json = BackendReturnObject.Flatten(bro.Rows());
+                for(int i=0; i<json.Count; ++i)
+                {
+                    // 데이터를 디시리얼라이즈 & 데이터 확인
+                    WeaponStat item = JsonMapper.ToObject<WeaponStat>(json[i].ToJson());
+                    Debug.Log(item.ToString());
+                }
+            });
+        },
+        () =>//6
+        {
+            SendQueue.Enqueue(Backend.GameData.Get, "weaponTest_JG", new Where(), 10, bro =>
+            {
+                if (bro.IsSuccess() == false)
+                {
+                    // 요청 실패 처리
+                    Debug.Log(bro);
+                    return;
+                }
+                Debug.Log("ggg:"+bro.GetReturnValue());
+                // 검색한 데이터의 모든 row의 inDate 값 확인
+                for (int i = 0; i < bro.Rows().Count; ++i)
+                {
+                    var inDate = bro.Rows()[i]["inDate"]["S"].ToString();
+                    Debug.Log(inDate);
+                }
+            });
+        },
+
+
+    };
+   
+
+    
+    public void IndexToExcute(int index)
+    {
+        
+        test[index]();
+    }
+}
