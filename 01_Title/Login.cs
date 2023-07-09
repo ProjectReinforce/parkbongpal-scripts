0edf4792db56674c8c18b90762daf5acc1863862
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2023-07-09 12:37:02 +0900

    feat: 신규 유저 데이터 삽입 기능 추가
    + 신규 유저 데이터 삽입 기능
    + 관련 코드 수정

--- 01_Title/Login.cs
+++ 01_Title/Login.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BackEnd;
 
 public class Login : MonoBehaviour
 {
     [SerializeField] GameObject LoginPopup;
+    
     public void TryToLoginWithToken()
     {
-        var bro = BackEnd.Backend.BMember.LoginWithTheBackendToken();
+        var bro = Backend.BMember.LoginWithTheBackendToken();
 
         if (bro.IsSuccess())
         {
@@ -27,9 +29,17 @@ public class Login : MonoBehaviour
         
         if(bro.IsSuccess())
         {
-            Debug.Log("게스트 로그인 성공!");
-            //UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene JG");
-            Utills.LoadScene("Scene1");
+            switch(int.Parse(bro.GetStatusCode()))
+            {
+                case 200:
+                    Debug.Log("게스트 로그인 성공!");
+                    Utills.LoadScene("Scene1");
+                    break;
+                case 201:
+                    Debug.Log("게스트 회원가입 성공!");
+                    InsertNewUserData();
+                    break;
+            }
         }
         else
         {
@@ -37,18 +47,47 @@ public class Login : MonoBehaviour
         }
     }
 
-  
+    void InsertNewUserData()
+    {
+        Param newParam = SetInitialParam();
+
+        var bro = Backend.GameData.Insert("UserData", newParam);
+
+        if(bro.IsSuccess())
+        {
+            Debug.Log("신규 유저 데이터 삽입 성공!");
+            Utills.LoadScene("Scene1");
+        }
+        else
+        {
+            Debug.LogError($"신규 유저 데이터 삽입 실패 : {bro}");
+        }
+    }
+
+    Param SetInitialParam()
+    {
+        Param param = new Param();
+        param.Add("level", 1);
+        param.Add("exp", 0);
+        param.Add("max_exp", 0);
+        param.Add("gold", 0);
+        param.Add("diamond", 0);
+        param.Add("weapon_soul", 0);
+        param.Add("ore", 0);
+
+        return param;
+    }
 
     public void OnClickLogout()
     {
-        BackEnd.Backend.BMember.Logout();
+        Backend.BMember.Logout();
         Utills.LoadScene("Start");
         // 액세스 토큰 삭제, 즉 토큰 로그인 불가 (로그인이 먼저 되어야함)
     }
 
     public void OnClickDeleteData()
     {
-        BackEnd.Backend.BMember.DeleteGuestInfo();
+        Backend.BMember.DeleteGuestInfo();
         // 서버 측 데이터는 삭제 안됨
     }
 }
