56e7c0a7c66a7bac36b1f31de480854ce75902e1
KongJin <pibrod@gmail.com>
2023-07-10 11:35:51 +0900

    feat: 씬1에서 데이터 로딩

--- Manager/GameManager.cs
+++ Manager/GameManager.cs
@@ -10,9 +10,11 @@ namespace Manager
         protected override void Awake()
         {
             base.Awake();
+            
+            BackendManager.Instance.searchFromMyIndate.Equal("owner_inDate", BackEnd.Backend.UserInDate);
 
-            Inventory inventory = new GameObject("Inventory").AddComponent<Inventory>();
-            Player player = new GameObject("Player").AddComponent<Player>();
+            Inventory inventory = new GameObject("Inventory_S").AddComponent<Inventory>();
+            Player player = new GameObject("Player_S").AddComponent<Player>();
             player.Initialize(inventory);
 
 
