adcefcb54101266de3f1a2536009d5dce1e49f6e
IJH_97 <dryer97@naver.com>
2023-07-15 14:11:53 +0900

    feat : IJH씬 수정, store스크립트 내용 초기화
    제목과 같은 내용

--- 03_Main/Store/Store.cs
+++ 03_Main/Store/Store.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using BackEnd;
 using LitJson;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Store:Manager.Singleton<Store>
 {
-   
+    
 }
