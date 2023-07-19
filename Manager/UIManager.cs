1eaed9b2c89f170a437fb03c0d2016589f3a89ac
legoPepsi <123460716+legoPepsi@users.noreply.github.com>
2023-07-19 21:01:47 +0900

    feat: 인벤토리, 강화 화면 추가사항

--- Manager/UIManager.cs
+++ Manager/UIManager.cs
@@ -3,9 +3,12 @@ namespace Manager
 {
     public class UIManager: Singleton<UIManager>
     {
+        [SerializeField] GameObject mineInfo;
         protected override void Awake()
         {
             base.Awake();
         }
+        public void MineOn() { mineInfo.SetActive(true); }
+        public void MineOff() {  mineInfo.SetActive(false); }
     }
 }
