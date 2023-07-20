eb5bf473e2b24448b3197981aae3ce972b00f0ea
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2023-07-20 13:21:54 +0900

    feat: 뽑기 기능 분리
    + 일반 무기 제작
    + 고급 무기 제작

--- 03_Main/Store/TMP_GetPercentage.cs
+++ 03_Main/Store/TMP_GetPercentage.cs
@@ -4,35 +4,74 @@ using UnityEngine;
 using BackEnd;
 using LitJson;
 
+public enum ProbabilityName
+{
+    NormalGacha,
+    AdvancedGacha
+}
+
+[RequireComponent(typeof(UnityEngine.UI.Button))]
 public class TMP_GetPercentage : MonoBehaviour
 {
-    public void GetPercentage()
+    [Header("버튼을 누르면 받아올 확률 정보의 이름 (뒤끝 - 확률관리 - 확률명)")]
+    [SerializeField] ProbabilityName targetProbabilityName;
+    [Header("수행할 뽑기 횟수")]
+    [Range(1, 100)]
+    [SerializeField] int excuteTime;
+    string targetProbabilityFieldId;
+    
+    void Start()
+    {
+        UnityEngine.UI.Button button = GetComponent<UnityEngine.UI.Button>();
+        button.onClick.AddListener(ExcuteGacha);
+    }
+
+    public void ExcuteGacha()
     {
         SendQueue.Enqueue(Backend.Probability.GetProbabilityCardListV2, callback =>
         {
             if(!callback.IsSuccess())
             {
                 Debug.LogError($"확률 정보 수신 실패 : {callback}");
+                // 경고 메시지 UI 출력
                 return;
             }
-            JsonData percentage = callback.FlattenRows();
-            // Debug.Log(percentage[0][0]);
-            
-            foreach(JsonData one in percentage)
-                Debug.Log($"{one["probabilityName"].ToString()} / {one["probabilityExplain"].ToString()} / {one["selectedProbabilityFileId"].ToString()}");
-        });
 
-        SendQueue.Enqueue(Backend.Probability.GetProbabilitys, "7937", 10, callback =>
+            JsonData probabiltyLists = callback.FlattenRows();
+            
+            foreach(JsonData probability in probabiltyLists)
+            {
+                Debug.Log($"{probability["probabilityName"].ToString()} / {probability["probabilityExplain"].ToString()} / {probability["selectedProbabilityFileId"].ToString()}");
+
+                if(probability["probabilityName"].ToString() == targetProbabilityName.ToString())
+                {
+                    targetProbabilityFieldId = probability["selectedProbabilityFileId"].ToString();
+                    GetProbabilitys();
+                    return;
+                }
+            }
+
+            Debug.LogError($"{targetProbabilityName.ToString()}과 일치하는 확률 정보가 서버에 존재하지 않습니다.");
+        });
+    }
+
+    void GetProbabilitys()
+    {
+        SendQueue.Enqueue(Backend.Probability.GetProbabilitys, targetProbabilityFieldId, excuteTime, callback =>
         {
             if(!callback.IsSuccess())
             {
                 Debug.LogError($"제작 실패 : {callback}");
+                // 경고 메시지 UI 출력
                 return;
             }
-            JsonData datas = callback.GetFlattenJSON()["elements"];
 
-            foreach(JsonData one in datas)
-                Debug.Log($"{one["num"].ToString()} / {one["name"].ToString()} / {one["percent"].ToString()}");
+            JsonData results = callback.GetFlattenJSON()["elements"];
+
+            foreach(JsonData result in results)
+            {
+                Debug.Log($"{result["num"].ToString()} / {result["name"].ToString()} / {result["percent"].ToString()}");
+            }
         });
     }
 }
