015524226cc71aa61a161ee7a42a1ff58add7a09
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2023-08-28 12:49:31 +0900

    fix : 승급, 일반 강화 UI 수정

--- 03_Main/Reinforce/PromoteUI.cs
+++ 03_Main/Reinforce/PromoteUI.cs
@@ -11,14 +11,38 @@ public class PromoteUI : ReinforceUIBase
     [SerializeField] Image currentRarityNameImage;
     [SerializeField] Text currentRarityNameText;
     [SerializeField] Image[] weaponIcons;
+    [SerializeField] Image[] weaponSlots;
     [SerializeField] MagicCarveButtonUI magicCarveButtonUI;
 
-    void UpdateWeaponIcon()
+    // todo: 리소스매니저에서 받아오도록 수정
+    [SerializeField] Sprite[] slotSprites;
+    Sprite basicSprite;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        basicSprite = weaponSlots[0].sprite;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        foreach (var item in weaponSlots)
+            item.sprite = basicSprite;
+    }
+
+    void UpdateWeaponImage()
     {
         Weapon weapon = reinforceManager.SelectedWeapon;
 
         foreach (var item in weaponIcons)
             item.sprite = weapon.sprite;
+
+        weaponSlots[0].sprite = slotSprites[weapon.data.rarity];
+        weaponSlots[1].sprite = slotSprites[weapon.data.rarity + 1];
+
         weaponNameText.text = weapon.name;
     }
 
@@ -32,7 +56,7 @@ public class PromoteUI : ReinforceUIBase
 
     protected override void UpdateInformations()
     {
-        UpdateWeaponIcon();
+        UpdateWeaponImage();
     }
 
     protected override void RegisterAdditionalButtonClickEvent()
@@ -60,31 +84,37 @@ public class PromoteUI : ReinforceUIBase
     {
         WeaponData weaponData = reinforceManager.SelectedWeapon.data;
 
+        weaponSlots[0].sprite = slotSprites[weaponData.rarity];
+        if (weaponData.rarity != (int)Rarity.legendary)
+            weaponSlots[1].sprite = slotSprites[weaponData.rarity + 1];
+        else
+            weaponSlots[1].sprite = slotSprites[weaponData.rarity];
+
         switch (weaponData.rarity)
         {
             case (int)Rarity.trash:
-                currentRarityNameImage.color = Color.black;
-                nextRarityNameImage.color = Color.gray;
+                currentRarityNameImage.color = Color.blue;
+                nextRarityNameImage.color = Color.cyan;
                 break;
             case (int)Rarity.old:
-                currentRarityNameImage.color = Color.gray;
-                nextRarityNameImage.color = Color.blue;
-                break;
-            case (int)Rarity.normal:
-                currentRarityNameImage.color = Color.blue;
-                nextRarityNameImage.color = Color.red;
-                break;
-            case (int)Rarity.rare:
-                currentRarityNameImage.color = Color.red;
+                currentRarityNameImage.color = Color.cyan;
                 nextRarityNameImage.color = Color.green;
                 break;
-            case (int)Rarity.unique:
+            case (int)Rarity.normal:
                 currentRarityNameImage.color = Color.green;
                 nextRarityNameImage.color = Color.yellow;
                 break;
-            case (int)Rarity.legendary:
+            case (int)Rarity.rare:
                 currentRarityNameImage.color = Color.yellow;
-                nextRarityNameImage.color = Color.yellow;
+                nextRarityNameImage.color = Color.magenta;
+                break;
+            case (int)Rarity.unique:
+                currentRarityNameImage.color = Color.magenta;
+                nextRarityNameImage.color = Color.red;
+                break;
+            case (int)Rarity.legendary:
+                currentRarityNameImage.color = Color.red;
+                nextRarityNameImage.color = Color.red;
                 break;
         }
         currentRarityNameText.text = Utills.CapitalizeFirstLetter(((Rarity)weaponData.rarity).ToString());
