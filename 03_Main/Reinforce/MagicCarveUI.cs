8a532b2a426ebfc581fbc5d17a31fc5a6b16a4c7
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2023-08-29 12:31:55 +0900

    fix : 마법 부여 UI 연동

--- 03_Main/Reinforce/MagicCarveUI.cs
+++ 03_Main/Reinforce/MagicCarveUI.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Manager;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MagicCarveUI : ReinforceUIBase
 {
-    [SerializeField] Text[] currentSkillTexts;
+    [SerializeField] Text[] skillDescriptionTexts;
+    [SerializeField] Text[] skillNameTexts;
+    [SerializeField] Image[] lockImages;
+    [SerializeField] Image[] skillIcons;
+    Sprite nullSprite;
     // GameObject[] newSkills;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        nullSprite = skillIcons[0].sprite;
+    }
 
     void UpdateSkill()
     {
         Weapon weapon = reinforceManager.SelectedWeapon;
 
-        currentSkillTexts[0].text = weapon is not null && weapon.data.magic[0] != -1 ? $"{(MagicType)weapon.data.magic[0]}" : "";
-        currentSkillTexts[1].text = weapon is not null && weapon.data.magic[1] != -1 ? $"{(MagicType)weapon.data.magic[1]}" : "";
+        if (weapon is null) return;
+
+        for (int i = 0; i < skillDescriptionTexts.Length; i++)
+        {
+            if (weapon.data.magic[i] != -1)
+            {
+                SkillData skillData = BackEndDataManager.Instance.skillDatas[weapon.data.magic[i]];
+                skillIcons[i].sprite = BackEndDataManager.Instance.GetSkill(weapon.data.magic[i]);
+                skillNameTexts[i].text = $"{skillData.skillName}";
+                skillDescriptionTexts[i].text = $"{skillData.description}";
+            }
+            else
+            {
+                skillIcons[i].sprite = nullSprite;
+                skillNameTexts[i].text = "";
+                skillDescriptionTexts[i].text = "";
+            }
+        }
+
+        // currentSkillTexts[0].text = weapon is not null && weapon.data.magic[0] != -1 ? $"{(MagicType)weapon.data.magic[0]}" : "";
+        // currentSkillTexts[1].text = weapon is not null && weapon.data.magic[1] != -1 ? $"{(MagicType)weapon.data.magic[1]}" : "";
     }
 
     protected override void UpdateCosts()
@@ -62,9 +93,26 @@ public class MagicCarveUI : ReinforceUIBase
         WeaponData selectedWeapon = reinforceManager.SelectedWeapon.data;
 
         if (selectedWeapon.rarity < (int)Rarity.rare)
+        {
+            lockImages[0].enabled = true;
+            lockImages[1].enabled = true;
+            skillIcons[0].enabled = false;
+            skillIcons[1].enabled = false;
             return false;
+        }
         else
+        {
+            lockImages[0].enabled = false;
+            lockImages[1].enabled = true;
+            skillIcons[0].enabled = true;
+            skillIcons[1].enabled = false;
+            if (selectedWeapon.rarity >= (int)Rarity.legendary)
+            {
+                lockImages[1].enabled = false;
+                skillIcons[1].enabled = true;
+            }
             return true;
+        }
     }
 
     protected override bool Checks()
