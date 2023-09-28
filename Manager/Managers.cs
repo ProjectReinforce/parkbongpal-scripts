3503aad5880435afe644dc2807e3f6ef27a122d7
IJH_97 <dryer97@naver.com>
2023-09-28 14:05:46 +0900

    설정 합치기, 로그인 연결씬 Main으로 변경

--- Manager/Managers.cs
+++ Manager/Managers.cs
@@ -118,7 +118,7 @@ public class Managers : MonoBehaviour
                     serverData.Initialize();
                 }
                 break;
-            case SceneName.R_Main_V6_JH:
+            case SceneName.R_Main_V6:
                 game.Set();
                 break;
         }
