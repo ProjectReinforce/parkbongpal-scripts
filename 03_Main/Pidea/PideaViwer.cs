cbf8fb3fe015251e6b9fe79146d6a3b00a88abd3
legoPepsi <123460716+legoPepsi@users.noreply.github.com>
2023-10-26 18:56:05 +0900

    design: 도감 선택한 토글만 본래의 색으로 보이도록 변경

--- 03_Main/Pidea/PideaViwer.cs
+++ 03_Main/Pidea/PideaViwer.cs
@@ -2,25 +2,25 @@
 
 public class PideaViwer:MonoBehaviour
 {
-    [SerializeField] UnityEngine.UI.Toggle trashToggle;
-    [SerializeField] RectTransform trashScrollView;
-    [SerializeField] UnityEngine.UI.Toggle pideaToggle;
-    [SerializeField] GameObject pideaView;
-
-    void OnEnable()
-    {
-        OpenSetting();
-    }
+    // 테스트중
+    //[SerializeField] UnityEngine.UI.Toggle trashToggle;
+    //[SerializeField] RectTransform trashScrollView;
+    //[SerializeField] UnityEngine.UI.Toggle pideaToggle;
+    //[SerializeField] GameObject pideaView;
+    //void OnEnable()
+    //{
+    //    OpenSetting();
+    //}
     void OnDisable()
     {
         Managers.Event.PideaViwerOnDisableEvent?.Invoke();
     }
-    void OpenSetting()
-    {
-        pideaToggle.isOn = true;
-        trashToggle.isOn = true;
-        float PosX = trashScrollView.anchoredPosition.x;
-        trashScrollView.anchoredPosition = new Vector2(PosX, 0);
-        pideaView.SetActive(true);
-    }
+    //void OpenSetting()
+    //{
+    //    pideaToggle.isOn = true;
+    //    trashToggle.isOn = true;
+    //    float PosX = trashScrollView.anchoredPosition.x;
+    //    trashScrollView.anchoredPosition = new Vector2(PosX, 0);
+    //    pideaView.SetActive(true);
+    //}
 }
