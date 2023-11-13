6c405518dbdd48d8d43fa32e563a90833266fe98
legoPepsi <123460716+legoPepsi@users.noreply.github.com>
2023-11-13 13:29:22 +0900

    feat: 도감 기본화면 세팅

--- 03_Main/Pidea/PideaViwer.cs
+++ 03_Main/Pidea/PideaViwer.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PideaViwer:MonoBehaviour
 {
+    [SerializeField] ToggleGroup topToggleGruop;
+    [SerializeField] ToggleGroup gradeToggleGruop;
+
     void OnEnable()
     {
-        // ClickTap 함수 사용
         Managers.Event.PideaOpenSetting?.Invoke();
+        topToggleGruop.transform.GetChild(0).GetComponent<Toggle>().isOn = true;
+        gradeToggleGruop.transform.GetChild(0).GetComponent<Toggle>().isOn = true;
     }
     void OnDisable()
     {
         Managers.Event.PideaViwerOnDisableEvent?.Invoke();
+
+        foreach (Toggle toggle in topToggleGruop.GetComponentsInChildren<Toggle>())
+        {
+            toggle.isOn = false;
+        }
+
+        foreach(Toggle one in gradeToggleGruop.GetComponentsInChildren<Toggle>())
+        {
+            one.isOn = false;
+        }
     }
 }
