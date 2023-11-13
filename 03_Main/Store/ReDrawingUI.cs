9657e473ca1df7c2e74b6fb81d09c2095492b698
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-11-13 14:11:58 +0900

    feat : Weapon Update & Add CutScene Control
    + 컷신 컨트롤 가능해짐
    => Store 함수 하나로 통일함
    + 무기 업데이트함
    => 컴프레셔 관련 값 None으로 바꿈
    + 비주얼 업데이트 사항
    => 17 18 19 20 33 46 47 인덱스 무기

--- 03_Main/Store/ReDrawingUI.cs
+++ 03_Main/Store/ReDrawingUI.cs
@@ -15,7 +15,7 @@ public class ReDrawingUI : MonoBehaviour
             reDrawingButton.onClick.AddListener(() => 
             {
                 Managers.UI.ClosePopup();
-                store.BatchDrawing(redrawingType);
+                store.ExpectManaufactureUI(redrawingType, redarwingCount);
             });
         }
         else
@@ -23,7 +23,7 @@ public class ReDrawingUI : MonoBehaviour
             reDrawingButton.onClick.AddListener(() => 
             {
                 Managers.UI.ClosePopup();
-                store.Drawing(redrawingType);
+                store.ExpectManaufactureUI(redrawingType, redarwingCount);
             });
         }
     }
--- 03_Main/Store/Store.cs
+++ 03_Main/Store/Store.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class Store : MonoBehaviour
 {
-    private GachaData[] gacharsPercents; // 가챠 확률에 대한 변수로 GachaData에 있는 구조체를 통해 등급에 대한 변수로 다가갈 수 있게 만듦
-    private int[][] percents; // 배열을 활용하여 가챠 확률과 가챠 등급을 배정하는 변수
-    [SerializeField] ManufactureResultUI manufactureUI; // SetInfo 함수를 통해 베이스 웨폰 데이터의 정보를 넘겨줌
-    [SerializeField] ManufactureResultUI manufactureOneUI; // 마찬가지로 정보를 넘겨주기 위해 만든 변수임
+    private GachaData[] gacharsPercents;
+    private int[][] percents;
+    [SerializeField] ManufactureResultUI manufactureUI;
+    [SerializeField] ManufactureResultUI manufactureOneUI;
+    [SerializeField] UnityEngine.UI.Button normalGacha;
+    [SerializeField] UnityEngine.UI.Button normalTenGacha;
+    [SerializeField] UnityEngine.UI.Button epicGacha;
+    [SerializeField] UnityEngine.UI.Button epicTenGacha;
+    [SerializeField] GameObject cutSceneControl;
 
     protected void Awake()
     {
-        gacharsPercents = Managers.ServerData.GachaDatas; // 서버데이터에 저장된 가챠 데이터를 받아와 가챠 퍼센트에 넣음
-        percents = new int[gacharsPercents.Length][]; // 배열로 선언한 변수의 행에 변수 가챠 퍼센트에 대한 길이를 넣음
-        for (int i = 0; i < gacharsPercents.Length; i++) // 가챠데이터에 대한 정보를 percents라는 변수에 담는 for문
+        gacharsPercents = Managers.ServerData.GachaDatas;
+        percents = new int[gacharsPercents.Length][];
+        for (int i = 0; i < gacharsPercents.Length; i++)
         {
             GachaData gachaData = gacharsPercents[i];
             percents[i] = new[] { gachaData.trash, gachaData.old, gachaData.normal, gachaData.rare, gachaData.unique, gachaData.legendary };
         }
 
+        normalGacha.onClick.AddListener(() => ExpectManaufactureUI(0, ONE));
+        normalTenGacha.onClick.AddListener(() => ExpectManaufactureUI(0, TEN));
+        epicGacha.onClick.AddListener(() => ExpectManaufactureUI(1, ONE));
+        epicTenGacha.onClick.AddListener(() => ExpectManaufactureUI(1, TEN));
     }
 
-    // 서버에서 받는 부분이 없음
-    const int COST_GOLD = 10000; // 고정된 변수를 통해 뽑기 당 필요한 재화 설정
+    const int COST_GOLD = 10000;
     const int COST_DIAMOND = 300;
-    private const int ONE = 1; // 임시적으로 작동하게 만듦
-    // todo : 개선 필요. 슬롯, 재화 체크를 상점에서 할 이유가 없음.
-    public void Drawing(int type) // 뽑기 시 사용하는 함수
+    private const int ONE = 1;
+    public void Drawing(int type)
     {
         if (Managers.Game.Inventory.CheckRemainSlots(ONE))
         {
@@ -62,16 +69,15 @@ public class Store : MonoBehaviour
                 SendChat.SendMessage($"레전드리 <color=red>{baseWeaponDatas[i].name}</color> 획득!");
         }
 
-        Managers.Game.Inventory.AddWeapons(baseWeaponDatas);    // 얻은 무기를 인벤토리에 추가함
-        manufactureOneUI.SetInfo(type, baseWeaponDatas);  // 얻은 무기에 대한 정보를 넘겨줌
+        Managers.Game.Inventory.AddWeapons(baseWeaponDatas);
+        manufactureOneUI.SetInfo(type, baseWeaponDatas); 
         Managers.UI.OpenPopup(manufactureOneUI.gameObject);
         if(manufactureOneUI.gameObject.activeSelf == true)
             manufactureOneUI.ManuFactureSpriteChange();
     }
 
-    private const int TEN = 10; // 무기제작 횟수를 변수로 저장함
-    // todo : 개선 필요. 슬롯, 재화 체크를 상점에서 할 이유가 없음.
-    public void BatchDrawing(int type) // 10번 뽑기 시 활용하는 함수
+    private const int TEN = 10;
+    public void BatchDrawing(int type)
     {
         if (Managers.Game.Inventory.CheckRemainSlots(TEN))
         {
@@ -116,5 +122,61 @@ public class Store : MonoBehaviour
         Managers.UI.OpenPopup(manufactureUI.gameObject);
         if(manufactureUI.gameObject.activeSelf == true)
             manufactureUI.ManuFactureSpriteChange();
+    }
+
+    public void ExpectManaufactureUI(int _type, int _count)
+    {
+        if (Managers.Game.Inventory.CheckRemainSlots(_count))
+        {
+            Managers.Alarm.Warning("인벤토리 공간이 부족합니다.");
+            return;
+        }
+
+        if (_type == 0) // 타입에 따라 뽑기를 진행함
+        {
+            if (!Managers.Game.Player.AddGold(-COST_GOLD))
+            {
+                Managers.Alarm.Warning("골드가 부족합니다.");
+                return;
+            }
+            Managers.Game.Player.TryProduceWeapon(_count);
+        }
+        else
+        {
+            if (!Managers.Game.Player.AddDiamond(-COST_DIAMOND))
+            {
+                Managers.Alarm.Warning("다이아몬드가 부족합니다.");
+                return;
+            }
+            Managers.Game.Player.TryAdvanceProduceWeapon(_count);
+        }
+
+        cutSceneControl.SetActive(true);
+
+        BaseWeaponData[] baseWeaponDatas = new BaseWeaponData[_count];
+        for (int i = 0; i < _count; i++)
+        {
+            Rarity rarity = (Rarity)Utills.GetResultFromWeightedRandom(percents[_type]);
+            baseWeaponDatas[i] = Managers.ServerData.GetBaseWeaponData(rarity);
+            if (rarity >= Rarity.legendary)
+                SendChat.SendMessage($"레전드리 <color=red>{baseWeaponDatas[i].name}</color> 획득!");
+        }
+
+
+        Managers.Game.Inventory.AddWeapons(baseWeaponDatas);
+        if(_count == ONE)
+        {
+            manufactureOneUI.SetInfo(_type, baseWeaponDatas);
+            Managers.UI.OpenPopup(manufactureOneUI.gameObject);
+            if (manufactureOneUI.gameObject.activeSelf == true)
+                manufactureOneUI.ManuFactureSpriteChange();
+        }
+        else
+        {
+            manufactureUI.SetInfo(_type, baseWeaponDatas);
+            Managers.UI.OpenPopup(manufactureUI.gameObject);
+            if (manufactureUI.gameObject.activeSelf == true)
+                manufactureUI.ManuFactureSpriteChange();
+        }
     }
 }
