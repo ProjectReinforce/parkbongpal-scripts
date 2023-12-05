1bd14bd397176827273593042f580396ec48fff4
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2023-12-05 14:19:36 +0900

    feat : 사이드 버튼 애니메이션 추가

--- 03_Main/Animation/SideButtonAnimation.cs
+++ 03_Main/Animation/SideButtonAnimation.cs
@@ -1,16 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class SideButtonAnimation : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 endPosition;
+    [SerializeField] float startDelay;
+    [SerializeField] float jumpDelay;
+    RectTransform rectTransform;
+    RectTransform imageRect;
+
+    void Start()
     {
-        
+        TryGetComponent(out rectTransform);
+        transform.GetChild(0).TryGetComponent(out imageRect);
+        endPosition = rectTransform.anchoredPosition;
+        rectTransform.anchoredPosition = startPosition;
+
+        Once();
     }
 
-    private void Update()
+    void Once()
     {
-        
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(startDelay);
+        seq.Append(rectTransform.DOAnchorPos3D(endPosition, 0.5f));
+        seq.OnComplete(() => Loop());
+    }
+
+    void Loop()
+    {
+        Vector3 endPos = imageRect.anchoredPosition;
+        endPos.y += 20f;
+
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(jumpDelay / 2f);
+        seq.Append(imageRect.DOAnchorPos(endPos, 0.3f));
+        seq.SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void Pop()
+    {
+        imageRect.DOPunchScale(Vector3.one * 1.05f, 0.3f, 5).SetLoops(1, LoopType.Yoyo);
     }
 }
