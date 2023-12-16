d570e547aea46d39fa3ec8022d10d298f7c0d764
legoPepsi <123460716+legoPepsi@users.noreply.github.com>
2023-12-16 11:28:25 +0900

    Desing: 튜토리얼 판넬 투명도 수정

--- 03_Main/Post/PostItemSlot.cs
+++ 03_Main/Post/PostItemSlot.cs
@@ -11,6 +11,6 @@ public class PostItemSlot : MonoBehaviour
     public void PostItemInitialized(PostItemData itemData)
     {
         itemImage.sprite = Managers.Resource.GetPostItem(itemData.itemId);
-        itemCount.text = itemData.itemCount.ToString();
+        //itemCount.text = Utills. itemData.itemCount.ToString();
     }
 }
